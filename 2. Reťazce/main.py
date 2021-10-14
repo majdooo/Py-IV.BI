48109dd69d9131fb637e55ccc829646a0942ca36
majdooo <91473696+majdooo@users.noreply.github.com>
2021-10-14 20:02:14 +0200

    Add files via upload

--- 2. Reťazce/main.py
+++ 2. Reťazce/main.py
@@ -1,1 +1,19 @@
+def pocet_suborov():
+    while True:
+        pocet = input("Zadaj počet súborov: ")
+        try:
+            return int(pocet)
+        except ValueError:
+            print("Musíš zadať číselnú hodnotu!")
 
+
+pocet_slov = []
+
+with open("basnicka.txt", encoding="utf-8") as subor:
+    for riadok in subor:
+        pocet_slov += riadok.split()
+
+for i in range(pocet_suborov()):
+    subor = open("Súbor" + str(i + 1) + ".txt", mode="w", encoding="utf-8")
+    subor.write(pocet_slov[i])
+    subor.close()
